9ea82c0f691c7b6e733b3b34d390762cd2ea8aae
Carlo Tacchella <carlo.tacchella@acs.it>
2024-07-19 12:08:02 +0200

    get rest api

--- booking/src/app/app.module.ts
+++ booking/src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './features/search/search.component';
 import { LoginComponent } from './features/login/login.component';
 import { CartComponent } from './features/cart/cart.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    LoginComponent,
-    CartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, SearchComponent, LoginComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- booking/src/app/features/search/search.component.ts
+++ booking/src/app/features/search/search.component.ts
@@ -1,10 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Hotel } from '../../model/hotel';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
-
+  /*
+  problems: second click on button does not change array
+  jsonserver error: query does not work 
+  */
+  text = 'Milan';
+  hotels: Hotel[] = [];
+  constructor(private http: HttpClient) {
+    this.searchHotels(this.text);
+  }
+  searchHotels(textToFind: string) {
+    this.text = textToFind;
+    console.log(textToFind);
+    this.http
+      .get<Hotel[]>('http://localhost:3000/hotels?q=' + textToFind)
+      .subscribe((result) => {
+        this.hotels = result;
+      });
+  }
 }
