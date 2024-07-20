a73a63a26049ac83276bd7776ff05b3e4b632dbe
Carlo Tacchella <carlo.tacchella@acs.it>
2024-07-20 12:15:46 +0200

    fix query

--- booking/src/app/features/search/search.component.ts
+++ booking/src/app/features/search/search.component.ts
@@ -12,18 +12,23 @@ export class SearchComponent {
   problems: second click on button does not change array
   jsonserver error: query does not work 
   */
-  text = 'Milan';
+  text = 'milan';
   hotels: Hotel[] = [];
+  active?: Hotel;
+
   constructor(private http: HttpClient) {
     this.searchHotels(this.text);
   }
   searchHotels(textToFind: string) {
     this.text = textToFind;
     console.log(textToFind);
-    this.http
-      .get<Hotel[]>('http://localhost:3000/hotels?q=' + textToFind)
-      .subscribe((result) => {
-        this.hotels = result;
-      });
+    const url = 'http://localhost:3000/hotels?city=' + textToFind;
+    this.http.get<Hotel[]>(url).subscribe((result) => {
+      this.hotels = result;
+    });
+    this.active = this.hotels[0];
+  }
+  setActive(hotel: Hotel) {
+    this.active = hotel;
   }
 }
